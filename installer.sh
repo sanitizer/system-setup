9bdff7e63acfaff52dfc746884f2b9465912acdc
sanitizer <pavel.volchak@gmail.com>
2020-10-11 17:06:52 -0600

    added linux setup initial script

---
+++ installer.sh
@@ -0,0 +1,22 @@
+sudo apt install vim;
+sudo apt-get update;
+sudo apt upgrade;
+sudo apt install zsh;
+sudo apt-get install powerline fonts-powerline curl git xclip nodejs npm openjdk-11-jdk;
+npm install -g @vue/cli;
+curl -sL https://firebase.tools | zsh;
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1;
+echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc";
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+sudo apt install apt-transport-https;
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -;
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list;
+sudo apt update;
+sudo apt install brave-browser;
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list';
+sudo apt-get install apt-transport-https;
+sudo apt-get update;
+sudo apt-get install code;
+
